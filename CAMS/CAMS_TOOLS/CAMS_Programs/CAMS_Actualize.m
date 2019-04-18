3101855d90add9f2ad0b2871acb06ab7309f22fd
Leonardo Pizarro <lpizarrobaeza@gmail.com>
2019-04-18 17:37:55 -0400

    Renamed

--- CAMS/CAMS_TOOLS/CAMS_Programs/CAMS_Actualize.m
+++ CAMS/CAMS_TOOLS/CAMS_Programs/CAMS_Actualize.m
@@ -1,73 +1,68 @@
-%Actualiser
+%actualize
 clear;
-%Ce spcript permet d'actualiser la banque de données du système video
-%A chaques nouvelles images sauvegardées dans le dossier
-%du NIVEAU 1 de la banque de données, le script détecte 
-%ces nouvelles images et lance les programmmes permettant d'obtenir:
+%This spcript is used to update the database of the video system
+% For each new image saved in the LEVEL 1 folder of the datastore, the script detects these new images and launches the programs to obtain:
+
+% LEVEL 2 of the databank
+% - the coordinates of the water line via CAMS_N2_Shoreline.mat
+% - the wave parameters via CAMS_N2_parameters.mat
+% - the directions from strikes via CAMS_N2_direction.mat
 %
-% NIVEAU 2 de la banque de données
-%   - les coordonnées de la ligne d'eau via CAMS_N2_Shoreline.mat
-%   - les paramètres de vague via CAMS_N2_parameters.mat
-%   - les direcions des vagues via CAMS_N2_direction.mat
-%
-% NIVEAU 3 de la banque de données
-%   - les moyennes journalières et mensuelles de HS,TP,Dir,Ligne_eau
-%   - les graphes associés
+% LEVEL 3 of the databank
+% - the daily and monthly averages of HS, TP, Dir, Ligne_eau
+% - the associated graphs
 
 %% CONFIGURATION:
 
 SITENAME = 'GRANDPOPO';
 SITECODE= 'GPP';
 
-%Emplacement du dossier "CAMS_DATA":
+%Folder location "CAMS_DATA":
 dirdata = 'D:\Tout le stage\CAMS\CAMS_DATA\';
 
-%Emplacement du dossier "CAMS_TOOLS":
+%Folder location "CAMS_TOOLS":
 dirtools = 'D:\Tout le stage\CAMS\CAMS_TOOLS\';
 
-%% IDENTIFICATION DES CHEMINS DES DOSSIERS DE LA BASE DE DONNEES
+%% IDENTIFICATION OF THE DATABASE PATHS OF THE DATABASE
 
-%Ajout des programmes de CAMS_TOOLS au Path de Matlab:
+% Adding CAMS_TOOLS Programs to the Matlab Path
 addpath(genpath(dirtools));
-%Chemin du dossier NIVEAU 1 de la base de données: dossier images
+%Folder path LEVEL 1 of the database: images folder
 dirN1 = [dirdata '02 - DATA\' SITENAME '\' SITECODE '_NIVEAU 1\' ];
-%Chemin du dossier NIVEAU 2
+% LEVEL 2 folder path 
 dirN2 = [dirdata '02 - DATA\' SITENAME '\' SITECODE '_NIVEAU 2\' ];
-%Chemin du dossier NIVEAU 3
+% LEVEL 3 folder path
 dirN3 = [dirdata '02 - DATA\' SITENAME '\' SITECODE '_NIVEAU 3\' ];
 
 
-%% ACTUALISATION DU NIVEAU 2
-disp('ACTUALISATION DU NIVEAU 2')
+%% UPDATE LEVEL 2
+disp('UPDATE LEVEL 2')
 %SHORELINE
-% disp('CALCUL DES POSITIONS DU TRAIT DE COTE')
+% disp('CALCULATION OF THE POSITIONS OF THE RAILWAY')
   dirN2_s = [dirN2 SITECODE '_Shoreline_Data\'];
 
-%On inventorie les dates pour lesquelles le trait de côte n'a pas été
-%calculé:
+%Dates are inventoried for which the coastline has not been calculated
 [ls_maj_s] = MAJBDD1(dirN1,dirN2_s);
 
-%On lance le calcul du trait de côte pour les jours listés dans ls_maj_s 
+% We start the calculation of the coastline for the days listed in ls_maj_s 
 CAMS_N2_Shoreline(dirN1,dirN2_s,ls_maj_s)
-disp('trait de côte à jour')
+disp('coastline up to date')
 
-%LES PARAMETRES DE VAGUE
-disp('CALCUL DES PARAMETRES DE VAGUE')
+%PARAMETERS OF WAVE
+disp('CALCULATION OF WAVE PARAMETERS')
  dirN2_p = [dirN2 SITECODE '_Parameters_Data\'];
 [ls_maj_p] = MAJBDD1(dirN1,dirN2_p);
 CAMS_N2_Parameters(dirN1,dirN2_p,ls_maj_p,0)
-disp('Paramètres de vague à jour');
+disp('Wave parameters up to date');
 
 
-%LA DIRECTION DES VAGUES
-disp('CALCUL DE LA DIRECTION DES VAGUES')
+%THE WAVE DIRECTION
+disp('CALCULATION OF THE DIRECTION OF THE WAVES')
  dirN2_d = [dirN2 SITECODE '_Direction_Data\'];
 [ls_maj_d] = MAJBDD1(dirN1,dirN2_d);
 CAMS_N2_Direction(dirN1,dirN2_d,ls_maj_d);
-disp('Directions à jour');
+disp('Directions to date');
 
-%% ACTUALISATION DU NIVEAU 3
-disp('ACTUALISATION DU NIVEAU 3')
+%% UPDATE LEVEL 3
+disp('UPDATE LEVEL 3')
 CAMS_N3_Parameters(dirN2_p,dirN2_d,dirN2_s,dirN3);
-
-
