ef307df650fd3209bece6ff04bbb0a11cc228b7f
Leonardo Pizarro <lpizarrobaeza@gmail.com>
2019-04-18 17:56:43 -0400

    a ingles

--- CAMS/CAMS_TOOLS/CAMS_Toolbox/startup.m
+++ CAMS/CAMS_TOOLS/CAMS_Toolbox/startup.m
@@ -1,6 +1,6 @@
-%Script lancé au demarrage de Matlab.
+%Script launched at the start of Matlab
 
-% %On lance les calculs de ligne d'eau
+% We start the calculations of water line
 %run('C:\Users\HP\Desktop\GrandPopo_Data\02-GrandPopo_Programs\02.4-Ligne_eau-PostProcess\PostProcess_GPP_ligne_eau_20151126.m')
 %run('C:\Users\HP\Desktop\GrandPopo_Data\02-GrandPopo_Programs\02.2-Wave_Parameters\ComputeGPPCharLongTerm_20150127_DayByDay.m')
 %run('C:\Users\HP\Desktop\GrandPopo_TOOLS\GPP_Programs\GPP_WaveParameters.m')
